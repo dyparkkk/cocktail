31c4d7df6f588df779c11021cda6ed0c8e2beffb
jwk34373 <88478357+jwk34373@users.noreply.github.com>
2022-06-29 15:47:06 +0900

    github action

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PROJECT_NAME="github_action"
-JAR_PATH="/home/ubuntu/github_action/*.jar"
+JAR_PATH="/home/ubuntu/github_action/build/libs/*.jar"
 DEPLOY_PATH="/cocktail/"
 DEPLOY_LOG_PATH="/data/logs/deploy.log"
 DEPLOY_ERR_LOG_PATH="/data/logs/deploy_err.log"
